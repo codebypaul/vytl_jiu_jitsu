9a730ed0a0c463253ca89753ebadfd28734c6b67
Paul Williams <paul.d.williamsxiv@gmail.com>
2023-11-05 18:15:30 -0500

    added sign-up, login, and logout pages, created a non-functional admin panel

--- vytl/website/urls.py
+++ vytl/website/urls.py
@@ -1,19 +1,23 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView
+from .views import CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns =  [
     # General site pages
     path('',views.home_view, name = 'home page'),
-    path('schedule',views.schedule, name = 'schedule'),
+    path('schedule/',views.schedule, name = 'schedule'),
+    path('about/',views.about, name = 'about'),
+    path('instructors/',views.instructors, name = 'instructors'),
+    path('pricing',views.pricing, name = 'pricing'),
     # path('about/',views.about,name='about'),
     # Authentication
-    path('',views.home_view, name = 'sign-up'),
+    path('sign-up/',RegisterPage.as_view(), name='sign-up'),
     path('login/',CustomLoginView.as_view(), name = 'login'),
     path('logout/',LogoutView.as_view(next_page='home page'), name = 'logout'),
     # Authenticated user pages
     path('profile/',views.profile, name = 'profile'),
+    path('admin-panel/',views.admin_panel, name = 'admin panel'),
 
 
 
--- vytl/website/views.py
+++ vytl/website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
+
 # Authentication
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 # General site pages
@@ -10,10 +15,36 @@ def home_view(request):
     return render(request, 'pages/home.html', context = {}, status =200 )
 
 def schedule(request):
-    return render(request, 'pages/schedule.html', context = {}, status =200 )
+    return render(request, 'pages/schedule.html', context = {}, status = 200 )
+
+def about(request):
+    return render(request, 'pages/about.html', context = {}, status = 200) 
+
+def instructors(request):
+    return render(request, 'pages/instructors.html', context = {}, status = 200 )
+
+def pricing(request):
+    return render(request, 'pages/pricing.html', context = {}, status = 200 )
 
 
 # Authentication pages
+class RegisterPage(FormView):
+    template_name =  'auth/register.html' 
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home page')
+
+    def form_valid(self, form):
+        user =form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home view')
+        return super(RegisterPage,self).get(*args,**kwargs)
+    
 class CustomLoginView(LoginView):
     template_name = 'auth/login.html'
     fields = '__all__'
@@ -25,4 +56,7 @@ class CustomLoginView(LoginView):
 # Authenticated user pages
 def profile(request):
     return render(request, 'user/profile.html')
+
 # Admin pages
+def admin_panel(request):
+    return render(request, 'user/admin_panel.html')
